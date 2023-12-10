f0ccbcaef130974018e54c4c4eaff1e509b54e92
arpit jain <jain.arpit@northeastern.edu>
2023-12-10 17:09:51 -0500

    Adding Footer and Header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import {Button} from "react-bootstrap";
 import { Entry } from './pages/entry/Entry.page';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 
 
 function App() {
   return (
     <div className="App">
       {/* <Entry /> */}
-      <DefaultLayout/>
+      <DefaultLayout>
+        <Dashboard/>
+      </DefaultLayout>
     </div>
   );
 }
